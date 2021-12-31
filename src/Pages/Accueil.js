ceb31bc636d1cf518f40b1cbed389b12e765ad6b
Ghiles52 <96773348+Ghiles52@users.noreply.github.com>
2021-12-31 16:05:09 +0100

    accueil

--- src/Pages/Accueil.js
+++ src/Pages/Accueil.js
@@ -1,57 +1,44 @@
 import React from "react";
 import { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../index.scss";
-import Inscription from "../Pages/Inscription";
-import Connexion from "../Pages/Connexion";
 import image from "../Images/accueil.png";
 import logo from "../Images/Logo.png";
 
 const Accueil = () => {
-  const [connexion, setconnexion] = useState(false);
-  const [inscription, setinscription] = useState(false);
   return (
-    <>
-      {inscription ? (
-        <Inscription />
-      ) : connexion ? (
-        <Connexion />
-      ) : (
-        <div className="container">
-          <div className="navbar_accueil">
-            <img className="logo" src={logo}></img>
-            <div className="boutons_accueil">
-              <button className="btn_accueil" id="inscrition_accueil">
-                Inscription
-              </button>
-              <button className="btn_accueil" id="connexion accueil">
-                Connexion
-              </button>
-            </div>
-          </div>
-          <div>
-            <div className="text_accueil">
-              <h1>
-                Bienvenue dans <span>REACT BANK!</span>
-              </h1>
-              <h2>
-                Gérez votre compte et effectuez des transactions en toute
-                sécurité.
-              </h2>
-              <button
-                className="btn"
-                onClick={() => {
-                  setinscription(true)
-                }}
-              >
-                Commencer
-              </button>
-            </div>
-            <img className="image_accueil" src={image}></img>
-          </div>
+    <div className="container">
+      <div className="navbar_accueil">
+        <img src={logo}></img>
+        <div className="btn_accueil">
+          <NavLink exact
+                to="/Inscription"
+                 id="inscrition_accueil">
+            Inscription
+          </NavLink> 
+          <NavLink exact
+                to="/Connexion"
+                  id="connexion_accueil">
+            Connexion
+          </NavLink>
         </div>
-      )}
-    </>
+      </div>
+      <h1>
+            Bienvenue dans <span>REACT BANK!</span>
+          </h1>
+          <img className="img_accueil" src={image}></img>
+      <div className="accueil">
+      
+        <div className="text_accueil">
+          <h3>
+            Gérez votre compte, effectuez des transactions et profitez de nos prêts en toute sécurité.
+          </h3>
+          <NavLink exact
+                to="/Inscription"
+                 id="commencer">Commencer</NavLink>
+        </div>
+      </div>
+    </div>
   );
 };
 
